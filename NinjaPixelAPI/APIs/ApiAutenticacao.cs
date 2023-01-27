d47ab52f22ffd86a02338af5f5b60bb35100b77d
Uesleyrodi <uesley_rsantos2@hotmail.com>
2023-01-27 08:14:30 -0300

    Reorganizando estruturação, criado as ramificações APIs e Features

--- NinjaPixelAPI/APIs/ApiAutenticacao.cs
+++ NinjaPixelAPI/APIs/ApiAutenticacao.cs
@@ -1,7 +1,57 @@
-﻿namespace NinjaPixelAPI.APIs
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+
+namespace NinjaPixelAPI.APIs
 {
     public class ApiAutenticacao
     {
+        //Endpoint
+        static string baseUrl = "http://localhost:3333";
 
+        //Rota
+        static string rotaAutenticacao = "/auth";
+
+        //Entrada de Dados
+        static string email = "uesley@ninjapixel.com";
+        static string password = "pwd123";
+
+        static RestClient client;
+        static RestRequest request;
+        static RestResponse response;
+
+        public static string GetToken()
+        {
+            // Inicializar client
+            InitApiAutenticacao();
+
+            //Representação do Enpoint
+            request = new RestRequest(rotaAutenticacao, Method.Post);
+            request.AddJsonBody(new
+            {
+                email,
+                password
+            });
+
+            response = client.Execute(request);
+
+            Console.WriteLine(response.Content);
+
+            Assert.IsTrue(response.Content.Contains("token"), "Verificar se existe a palavra token no retorno do response");
+
+            //Neste momento estou selecionando o token no retorno do response
+            string token = JObject.Parse(response.Content).SelectToken("token").ToString();
+
+            Console.WriteLine(token);
+
+            return token;
+        }
+        public static void InitApiAutenticacao()
+        {
+            //Representação da API
+            client = new RestClient(baseUrl);
+            client.AddDefaultHeader("Content-Type", "application/json");
+        }
     }
 }
---
+++ NinjaPixelAPI/Feature/AutenticacaoTeste.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NinjaPixelAPI.APIs;
+
+namespace NinjaPixelAPI.Feature
+{
+    [TestClass]
+    public class Autenticacao
+    {
+        [TestMethod]
+        public void AutenticacaoTokenTest()
+        {
+            ApiAutenticacao.GetToken();
+        }
+    }
+}
